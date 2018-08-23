4db0b462010da206c02bf6ae210e68c7135b726c
Olivier J <olivier.jarguel@gmail.com>
2018-08-23 11:25:29 +0200

    angular learning start with great demos exercise with TeamMood

---
+++ ng-demos/src/app/mood/mood-face/mood-face.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-mood-face',
+  templateUrl: './mood-face.component.html',
+  styleUrls: ['./mood-face.component.css']
+})
+export class MoodFaceComponent implements OnInit {
+
+  @Input() mood: string
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  isHappy() {
+
+    return this.mood === 'happy';
+  }
+
+  isSoso() {
+    return this.mood === 'soso';
+  }
+
+  isAngry() {
+    return this.mood === 'angry';
+  }
+
+
+}
